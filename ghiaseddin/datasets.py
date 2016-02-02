634a58559a54c09275768ed7987434b8f5d2585d
Yasser Souri <yassersouri@gmail.com>
2016-02-02 23:25:33 +0330

    prepend `_` to private variable names

--- ghiaseddin/datasets.py
+++ ghiaseddin/datasets.py
@@ -5,52 +5,56 @@ import utils
 import numpy as np
 
 
+class DatasetGenerator(object):
+    pass
+
+
 class DatasetHelper(object):
     """
     Base class for a dataset helper. Implements functionality while subclasses will focus on loading
     the data into the desired format.
 
     This helper needs the following properties to successfully perform the necessary actions:
-        1. ATT_NAMES: It is a 1 dimensional list or list-like object, containing string names for the attributes in the dataset.
-        2. image_adresses: It is a 1 dimentional list or list-like object, containing absolute image address for each image in the dataset.
-        3. train_pairs: It is a (n x 2) array where n in the number of training pairs and they contain index of the images as the image
-        address is specified with that index in image_adresses.
-        4. train_targets: It is a (n) array where n in the number of training pairs and contains the target posterior for our method
+        1. _ATT_NAMES: It is a 1 dimensional list or list-like object, containing string names for the attributes in the dataset.
+        2. _image_adresses: It is a 1 dimentional list or list-like object, containing absolute image address for each image in the dataset.
+        3. _train_pairs: It is a (n x 2) array where n in the number of training pairs and they contain index of the images as the image
+        address is specified with that index in _image_adresses.
+        4. _train_targets: It is a (n) array where n in the number of training pairs and contains the target posterior for our method
         ($\in [0, 1]$).
-        5. test_pairs: Similar to train_pairs but for testing pairs.
-        6. test_targets: Similar to train_targets but for for testing pairs.
+        5. _test_pairs: Similar to _train_pairs but for testing pairs.
+        6. _test_targets: Similar to _train_targets but for for testing pairs.
 
     Each dataset helper needs to implement its __init__ function which fills the above properties according to the way this data is stored
     on disk.
     """
-    ATT_NAMES = None
-    train_pairs = None
-    train_targets = None
-    test_pairs = None
-    test_targets = None
-    image_adresses = None
+    _ATT_NAMES = None
+    _train_pairs = None
+    _train_targets = None
+    _test_pairs = None
+    _test_targets = None
+    _image_adresses = None
 
     def __init__(self, root, attribute_index):
         self.root = root
         self.attribute_index = attribute_index
-        assert 0 <= attribute_index < len(self.ATT_NAMES)
+        assert 0 <= attribute_index < len(self._ATT_NAMES)
 
     def show_pair(self, pair_id, test=False):
         """
         Shows pairs of images in the dataset and their annotation (target) for the set attribute.
         """
-        pair = self.test_pairs[pair_id, :] if test else self.train_pairs[pair_id, :]
-        target = self.test_targets[pair_id] if test else self.train_targets[pair_id]
+        pair = self._test_pairs[pair_id, :] if test else self._train_pairs[pair_id, :]
+        target = self._test_targets[pair_id] if test else self._train_targets[pair_id]
 
-        img1_path = self.image_adresses[pair[0]]
-        img2_path = self.image_adresses[pair[1]]
+        img1_path = self._image_adresses[pair[0]]
+        img2_path = self._image_adresses[pair[1]]
 
         if target > 0.5:
-            print 'A is more %s than B (t: %2.2f)' % (self.ATT_NAMES[self.attribute_index], target)
+            print 'A is more %s than B (t: %2.2f)' % (self._ATT_NAMES[self.attribute_index], target)
         elif target < 0.5:
-            print 'A is less %s than B (t: %2.2f)' % (self.ATT_NAMES[self.attribute_index], target)
+            print 'A is less %s than B (t: %2.2f)' % (self._ATT_NAMES[self.attribute_index], target)
         else:
-            print 'A is the same as B in %s (t: %2.2f)' % (self.ATT_NAMES[self.attribute_index], target)
+            print 'A is the same as B in %s (t: %2.2f)' % (self._ATT_NAMES[self.attribute_index], target)
 
         fig = plt.figure(figsize=(8, 4))
         ax1 = fig.add_subplot(121)
@@ -73,7 +77,7 @@ class DatasetHelper(object):
 class Zappos50K1(DatasetHelper):
     """The dataset helper class for Zappos50K-1, the coarse version of the dataset."""
 
-    ATT_NAMES = ['open', 'pointy', 'sporty', 'comfort']
+    _ATT_NAMES = ['open', 'pointy', 'sporty', 'comfort']
 
     def __init__(self, root, attribute_index, split_index):
         super(Zappos50K1, self).__init__(root, attribute_index)
@@ -93,15 +97,15 @@ class Zappos50K1(DatasetHelper):
         image_pairs_order = labels_file['mturkOrder'].flatten()[attribute_index].astype(int)
 
         # create placeholders
-        self.train_pairs = np.zeros((len(train_index), 2), dtype=np.int)
-        self.train_targets = np.zeros((len(train_index),), dtype=np.float32)
-        self.test_pairs = np.zeros((len(test_index), 2), dtype=np.int)
-        self.test_targets = np.zeros((len(test_index),), dtype=np.float32)
+        self._train_pairs = np.zeros((len(train_index), 2), dtype=np.int)
+        self._train_targets = np.zeros((len(train_index),), dtype=np.float32)
+        self._test_pairs = np.zeros((len(test_index), 2), dtype=np.int)
+        self._test_targets = np.zeros((len(test_index),), dtype=np.float32)
 
         # fill place holders
-        self.image_adresses = [os.path.join(images_path, p[0]) for p in imagepath_info]
-        self._fill_pair_target(train_index, image_pairs_order, self.train_pairs, self.train_targets)
-        self._fill_pair_target(test_index, image_pairs_order, self.test_pairs, self.test_targets)
+        self._image_adresses = [os.path.join(images_path, p[0]) for p in imagepath_info]
+        self._fill_pair_target(train_index, image_pairs_order, self._train_pairs, self._train_targets)
+        self._fill_pair_target(test_index, image_pairs_order, self._test_pairs, self._test_targets)
 
     def _fill_pair_target(self, indexes, pair_order, pairs, targets):
         for i, id in enumerate(indexes):
