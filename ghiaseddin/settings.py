6226facddb571c2a93aec7e3a9ddb25051ed4ead
Yasser Souri <yassersouri@gmail.com>
2016-02-21 00:27:28 +0330

    change weights variable name

--- ghiaseddin/settings.py
+++ ghiaseddin/settings.py
@@ -3,8 +3,8 @@ import os
 data_root = os.path.join(os.path.expanduser('~'), 'ghiaseddin')
 model_root = os.path.join(data_root, 'models')
 
-googlenet = os.path.join(model_root, 'blvc_googlenet.pkl')
-googlenet = os.path.join(model_root, 'vgg16.pkl')
+googlenet_weights = os.path.join(model_root, 'blvc_googlenet.pkl')
+vgg16_weights = os.path.join(model_root, 'vgg16.pkl')
 
 result_models_root = os.path.join(model_root, 'results')
 zappos_result_models_root = os.path.join(result_models_root, 'zappos')
