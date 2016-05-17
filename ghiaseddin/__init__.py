254186582010f1204c41ea023c59e99fc0132ea4
Yasser Souri <yassersouri@gmail.com>
2016-05-17 15:14:06 +0430

    remove ranker which is not yet implemented

--- ghiaseddin/__init__.py
+++ ghiaseddin/__init__.py
@@ -2,8 +2,7 @@ from extractors import VGG16, GoogLeNet
 import utils
 import settings
 from datasets import Zappos50K1
-from ranker import Ghiaseddin
 
 
 __version__ = "0.1"
-__all__ = ["VGG16", "GoogLeNet", "Zappos50K1", "Ghiaseddin", "settings"]
+__all__ = ["VGG16", "GoogLeNet", "Zappos50K1", "settings"]
